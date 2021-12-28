cc568c83d77ff410eda64333bed061c433271193
njw9108 <89832278+njw9108@users.noreply.github.com>
2021-12-28 14:14:32 +0900

    post repository imple 작성

---
+++ jet_news_clone/lib/data/repository/post_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:jet_news_clone/core/result.dart';
+import 'package:jet_news_clone/data/data_source/news_data_source.dart';
+import 'package:jet_news_clone/domain/model/post.dart';
+import 'package:jet_news_clone/domain/model/posts_feed.dart';
+import 'package:jet_news_clone/domain/repository/posts_repository.dart';
+
+class PostRepositoryImpl implements PostsRepository {
+  final NewsDataSource _dataSource = NewsDataSource();
+
+  @override
+  Future<Result<Post>> getPost(String? postId) {
+    // TODO: implement getPost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<PostsFeed>> getPostsFeed() {
+    return _dataSource.getPostsFeed();
+  }
+
+  @override
+  void toggleFavorite(String postId) {
+    // TODO: implement toggleFavorite
+  }
+  
+}
