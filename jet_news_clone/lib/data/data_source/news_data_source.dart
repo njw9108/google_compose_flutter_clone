1ea2c1e6104b4be9546fb36a00c2081fdd85ba95
njw9108 <89832278+njw9108@users.noreply.github.com>
2021-12-28 14:08:44 +0900

    data source 변경  - getPost 삭제  - getPostsFeed 작성

--- jet_news_clone/lib/data/data_source/news_data_source.dart
+++ jet_news_clone/lib/data/data_source/news_data_source.dart
@@ -3,6 +3,7 @@ import 'package:jet_news_clone/core/result.dart';
 import 'dart:convert';
 
 import 'package:jet_news_clone/domain/model/post.dart';
+import 'package:jet_news_clone/domain/model/posts_feed.dart';
 
 class NewsDataSource {
   static String baseUrl =
@@ -13,13 +14,31 @@ class NewsDataSource {
   NewsDataSource({http.Client? client}) : _client = client ?? http.Client();
 
   //Get a specific JetNews post.
-  Future<Result<List<Post>>> getPost(String? postId) async {
+  Future<Result<Post>> getPost(String? postId) async {
+    throw UnimplementedError();
+  }
+
+  //Get JetNews posts.
+  Future<Result<PostsFeed>> getPostsFeed() async {
     try {
       final response = await _client.get(Uri.parse(baseUrl));
       Iterable jsonResponse = jsonDecode(response.body);
-      return Result.success(jsonResponse.map((e) => Post.fromJson(e)).toList());
+      List<Post> totalPosts =
+          jsonResponse.map((e) => Post.fromJson(e)).toList();
+
+      PostsFeed feed = PostsFeed(
+        highlightedPost: totalPosts[3],
+        recommendedPosts: [totalPosts[0], totalPosts[1], totalPosts[2]],
+        popularPosts: [totalPosts[4], totalPosts[5], totalPosts[6]],
+        recentPosts: [totalPosts[7], totalPosts[8], totalPosts[8]],
+      );
+
+      return Result.success(feed);
     } on Exception catch (e) {
       return Result.error(e.toString());
     }
   }
+
+  //Toggle a postId to be a favorite or not.
+  void toggleFavorite(String postId) {}
 }
