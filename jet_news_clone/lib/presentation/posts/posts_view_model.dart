bcf258c01572d57ac67feb774891ed8a3c9e189f
njw9108 <89832278+njw9108@users.noreply.github.com>
2021-12-28 14:46:18 +0900

    posts view model 정의

---
+++ jet_news_clone/lib/presentation/posts/posts_view_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:jet_news_clone/domain/use_case/get_post_feed_use_case.dart';
+import 'package:jet_news_clone/presentation/posts/posts_state.dart';
+
+class PostViewModel with ChangeNotifier {
+  final GetPostFeedUseCase _getPostFeed;
+
+  PostViewModel(this._getPostFeed);
+
+  PostsState _state = PostsState();
+  PostsState get state => _state;
+
+  void fetchPosts() async {
+    final result = await _getPostFeed();
+    result.when(success: (feed) {
+      _state = state.copyWith(feed: feed);
+    }, error: (message) {
+      print(message);
+    });
+    notifyListeners();
+  }
+
+}
