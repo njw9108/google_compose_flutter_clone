04dba240fc1f3bf300a5de331db8dba167ac4297
njw9108 <89832278+njw9108@users.noreply.github.com>
2021-12-29 12:10:31 +0900

    post detail page 수정

--- jet_news_clone/lib/presentation/posts/detail_page/post_detail_page.dart
+++ jet_news_clone/lib/presentation/posts/detail_page/post_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jet_news_clone/domain/model/paragraph.dart';
 import 'package:jet_news_clone/domain/model/post.dart';
 
 class PostDetailPage extends StatelessWidget {
@@ -59,10 +60,63 @@ class PostDetailPage extends StatelessWidget {
                       ),
                     )
                   : const Text(''),
+              const Padding(padding: EdgeInsets.all(8)),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.account_circle,
+                    size: 55,
+                  ),
+                  const Padding(padding: EdgeInsets.all(4)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        post.metadata.author.name,
+                        style: const TextStyle(fontSize: 17),
+                      ),
+                      Text(
+                        post.metadata.date +
+                            ' - ' +
+                            '${post.metadata.readTimeMinutes} min read',
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const Padding(padding: EdgeInsets.all(8)),
+              ...post.paragraphs.map((e) => buildContent(e)).toList(),
+              const Padding(padding: EdgeInsets.all(16)),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget buildContent(Paragraph paragraph) {
+    switch(paragraph.type) {
+
+      case ParagraphType.Title:
+        return const Text('title');
+      case ParagraphType.Caption:
+        return const Text('Caption');
+      case ParagraphType.Header:
+        return const Text('header');
+      case ParagraphType.Subhead:
+        return const Text('subhead');
+      case ParagraphType.Text:
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Text(paragraph.text,style: const TextStyle(fontSize: 22),),
+        );
+      case ParagraphType.CodeBlock:
+        return const Text('Code block');
+      case ParagraphType.Quote:
+        return const Text('quote');
+      case ParagraphType.Bullet:
+        return const Text('bullet');
+    }
   }
 }
