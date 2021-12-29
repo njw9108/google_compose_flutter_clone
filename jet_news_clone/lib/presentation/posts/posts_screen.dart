2e67f91c00f2681f9d5c79290bb42a4b5a6818ac
njw9108 <89832278+njw9108@users.noreply.github.com>
2021-12-29 15:36:47 +0900

    Drawer 수정

--- jet_news_clone/lib/presentation/posts/posts_screen.dart
+++ jet_news_clone/lib/presentation/posts/posts_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jet_news_clone/presentation/posts/components/highlight_post_widget.dart';
 import 'package:jet_news_clone/presentation/posts/components/popular_post_widget.dart';
@@ -5,6 +6,7 @@ import 'package:jet_news_clone/presentation/posts/components/recent_post_widget.
 import 'package:jet_news_clone/presentation/posts/components/recommended_post_widget.dart';
 import 'package:jet_news_clone/presentation/posts/detail_page/post_detail_page.dart';
 import 'package:jet_news_clone/presentation/posts/posts_view_model.dart';
+import 'package:jet_news_clone/ui/ui.dart';
 import 'package:provider/provider.dart';
 
 class PostsScreen extends StatefulWidget {
@@ -30,7 +32,61 @@ class _PostsScreenState extends State<PostsScreen> {
     final state = viewModel.state;
 
     return Scaffold(
-      drawer: const Drawer(),
+      drawer: SafeArea(
+          child: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const ListTile(
+              leading: Icon(Icons.beach_access_rounded),
+              title: Text(
+                'Jet News',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Divider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                tileColor: state.selectedDrawer == DrawerType.home
+                    ? Colors.pinkAccent
+                    : Colors.transparent,
+                onTap: () {
+                  viewModel.onDrawerSelectChange(DrawerType.home);
+                  Navigator.of(context).pop();
+                },
+                contentPadding: EdgeInsets.zero,
+                leading: const Icon(Icons.home),
+                title: const Text(
+                  'Home',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                tileColor: state.selectedDrawer == DrawerType.interest
+                    ? Colors.pinkAccent
+                    : Colors.transparent,
+                onTap: () {
+                  viewModel.onDrawerSelectChange(DrawerType.interest);
+                  Navigator.of(context).pop();
+                },
+                contentPadding: EdgeInsets.zero,
+                leading: const Icon(Icons.message_outlined),
+                title: const Text(
+                  'Interest',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
       appBar: AppBar(
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
