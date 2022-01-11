4ca9ae2188cf35f9f7360334a2541d6601a00a08
njw9108 <89832278+njw9108@users.noreply.github.com>
2022-01-11 11:49:16 +0900

    data 객체 정의

---
+++ jet_chat_clone_2/lib/domain/model/message.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'message.freezed.dart';
+
+part 'message.g.dart';
+
+@freezed
+class Message with _$Message {
+  factory Message({
+    required String author,
+    required String content,
+    required int timestamp,
+    String? authorImageUrl,
+  }) = _Message;
+
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
+}
