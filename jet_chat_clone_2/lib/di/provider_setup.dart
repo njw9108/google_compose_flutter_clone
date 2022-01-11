62b8895e0b12b5c725f42b7424619d40fb358e2d
njw9108 <89832278+njw9108@users.noreply.github.com>
2022-01-11 13:15:02 +0900

    provider setup 정의

---
+++ jet_chat_clone_2/lib/di/provider_setup.dart
@@ -0,0 +1,25 @@
+import 'package:jet_chat_clone_2/data/data_source/message_data_repository.dart';
+import 'package:jet_chat_clone_2/data/repository/message_repository_impl.dart';
+import 'package:jet_chat_clone_2/domain/use_case/load_history_use_case.dart';
+import 'package:jet_chat_clone_2/domain/use_case/load_user_profiles_use_case.dart';
+import 'package:jet_chat_clone_2/domain/use_case/message_use_case.dart';
+import 'package:jet_chat_clone_2/domain/use_case/receive_message_use_case.dart';
+import 'package:jet_chat_clone_2/domain/use_case/send_message_use_case.dart';
+import 'package:jet_chat_clone_2/presentation/chat/chat_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  final dataSource = MessageDataSource();
+  final repository = MessageRepositoryImpl(dataSource);
+  final messageUseCase = MessageUseCase(
+      loadHistory: LoadHistoryUseCase(repository),
+      loadUserProfiles: LoadUserProfilesUseCase(repository),
+      receiveMessage: ReceiveMessageUseCase(repository),
+      sendMessage: SendMessageUseCase(repository));
+  return [
+    ChangeNotifierProvider<ChatViewModel>(
+      create: (context) => ChatViewModel(messageUseCase),
+    )
+  ];
+}
